9082dd9c985a5aecf48ac3f24d92e1f18213c726
Javier Perez <javier21perez98@gmail.com>
2021-03-25 18:36:43 -0700

    created color updater for time blocks

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // This displays a live current date using moment in Name of the day and month. 
 // The day and year number and time in hours, minutes and seconds.
+
 function date() {var currentDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(currentDay);
 }
 setInterval(date, 1000);
 
-// past
-// present
-// future
+var blockHour = 9;
+var hours = $("textarea");
+var blockTime = moment().format("H");
+
+for (i=0; i < 9; i+=1) {
+    if (blockHour > blockTime){
+        hours.eq(i).addClass("future");
+    }
+    if (blockHour == blockTime){
+        hours.eq(i).addClass("present");
+    }
+    if (blockHour < blockTime){
+        hours.eq(i).addClass("past");
+    }
+    blockHour+=1;
+}
