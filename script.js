a4eb2c2fd6852af25d2a161dc00c17d992ad8673
Javier Perez <javier21perez98@gmail.com>
2021-03-25 20:37:39 -0700

    save button saves 9am event

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // This displays a live current date using moment in Name of the day and month. 
 // The day and year number and time in hours, minutes and seconds.
-
 function date() { var currentDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(currentDay);
 }
 setInterval(date, 1000);
 
+// backgroundcolor updater
 var blockHour = 9;
 var hours = $("textarea");
 var blockTime = moment().format("H");
@@ -23,12 +23,36 @@ for (i=0; i < 9; i+=1) {
     blockHour+=1;
 }
 
+// save button
 var buttonArea = document.querySelector(".container");
 
 buttonArea.addEventListener("click", function(event) {
     var saveBtn = event.target;
-    if (saveBtn.matches("i")||("button")) {
-      alert("Works")
-      
+    if (saveBtn.matches("button")) {
+        event.preventDefault();
+        alert("Works")
+        saveEvent();
+        renderEvents();
     }
 });
+
+var events = document.querySelector(".description");
+// save events
+function saveEvent() {
+    var plannerEvents = events.value;
+    localStorage.setItem("plannerEvents", JSON.stringify(plannerEvents));
+}
+
+function renderEvents() {
+  var savedEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+  if (savedEvents !== null) {
+    document.querySelector(".description").innerHTML = savedEvents;
+  }else {
+    return;
+  }
+}
+
+function init() {
+  renderEvents();
+}
+init();
