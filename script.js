43ba6d11fe71a089ee6e3ddb73b33364a7e958cf
Javier Perez <javier21perez98@gmail.com>
2021-03-25 19:52:54 -0700

    save button works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // This displays a live current date using moment in Name of the day and month. 
 // The day and year number and time in hours, minutes and seconds.
 
-function date() {var currentDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+function date() { var currentDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(currentDay);
 }
 setInterval(date, 1000);
@@ -22,3 +22,13 @@ for (i=0; i < 9; i+=1) {
     }
     blockHour+=1;
 }
+
+var buttonArea = document.querySelector(".container");
+
+buttonArea.addEventListener("click", function(event) {
+    var saveBtn = event.target;
+    if (saveBtn.matches("i")||("button")) {
+      alert("Works")
+      
+    }
+});
